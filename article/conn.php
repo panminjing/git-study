be269e771ac38f3950b2784543c760b5d8e42d4a
pmjhcf <1205457431@qq.com>
2016-03-20 16:55:00 +0800

    sql_gai1

--- article/conn.php
+++ article/conn.php
@@ -1,11 +1,11 @@
 <?php 
-	$url = " w.rdc.sae.sina.com.cn:3307 ";
-	$root = "1205457431@qq.com";
-	$pwd = "pmj5201314123";
-	@mysql_connect($url, $root, $pwd) or die("数据库连接失败");
+	// $url = " w.rdc.sae.sina.com.cn:3307 ";
+	// $root = "1205457431@qq.com";
+	// $pwd = "pmj5201314123";
+	@mysql_connect(SAE_MYSQL_HOST_M .':'. SAE_MYSQL_PORT, SAE_MYSQL_USER, SAE_MYSQL_PASS) or die("数据库连接失败");
 
 	header("Content-Type:text/html;charset=utf8");
-	mysql_select_db("app_swiper");
+	mysql_select_db(SAE_MYSQL_DB);
 	mysql_query("set names utf8");
 
 ?>
